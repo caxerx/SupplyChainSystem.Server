0e124f86447f5aa71ca5386a8628a7ecdaaef8a3
caxerx <webmaster@caxerx.com>
2018-05-31 20:56:05 +0800

    Add stock item

---
+++ SupplyChainSystem.Server/Models/StockItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+
+namespace SupplyChainSystem.Server.Models
+{
+    public class StockItem
+    {
+        [ForeignKey("Stock")] public int StockId { get; set; }
+        [JsonIgnore] public Stock Stock { get; set; }
+
+        public int VirtualItemId { get; set; }
+        [JsonIgnore] public VirtualItem VirtualItem { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
